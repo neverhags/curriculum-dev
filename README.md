4c36955dda5f07d71d31cd96dd5c1e3001f227be
neverhags <neverhags@gmail.com>
2019-08-15 04:36:50 -0500

    desplegando servidor test

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# Webiste on vuejs to practice
+# Werbiste on vuejs from scratch... a little practice
 
-to view results: https://neverhags.github.io
+Steps to create this site:
+
+Create a .gitignore & package.json
+    
+    $ echo "node_modules" > .gitignore
+    $ echo "{}" > package.json
+
+Installing our application dependencies:
+
+    $ npm install --save vue vue-router
+
+Installing webpack:
+
+    $ npm install --save-dev webpack webpack-cli
+    
+    
+    
+    
---
+++ src/components/Home/WelcomeMessage.vue
@@ -0,0 +1,175 @@
+<template>
+  <div>
+    <h1 class="c42" id="h.houhvypg736s"><span class="c33 c29"># </span><span class="c34 c29">H</span><span
+            class="c29 c33">eilner A. </span><span class="c29 c34">G</span><span class="c2">arcia S.</span></h1>
+    <p class="c10 c47"><span class="c21 c29 c35 c26">**Desarrollador Sr. FullStack**</span></p>
+    <h2 class="c7" id="h.wjv0h1w2o316"><span class="c4">### &Uacute;LTIMAS EXPERIENCIAS LABORALES:</span></h2>
+    <p class="c8 c10"><span class="c3">2018-2019</span></p>
+    <h3 class="c7" id="h.t2n5jur0w54e"><span class="c29 c40">### </span><span class="c13">Q-enta</span></h3>
+    <p class="c8 c10"><span class="c5">**Ingeniero Desarrollador Full Stack**</span></p>
+    <p class="c8 c12"><span class="c15">Dise&ntilde;ar y codificar sitios web y aplicaciones, definir interfaces gr&aacute;ficas, definir y desarrollar pruebas y soportes de aplicaciones, definir normas y est&aacute;ndares de calidad de las aplicaciones desarrolladas, hacer la documentaci&oacute;n del c&oacute;digo de todas las aplicaciones realizadas y las existentes en la compa&ntilde;&iacute;a, responder las inquietudes a los problemas de tecnolog&iacute;a. Actualmente &ldquo;clusterizando&rdquo; los servicios en AWS EKS</span>
+    </p>
+    <hr>
+    <p class="c16 c22"><span class="c3"></span></p>
+    <p class="c8 c10"><span class="c3">2017-2018</span></p>
+    <h3 class="c7" id="h.swuk05wqj945"><span class="c11">## </span><span class="c21 c40 c29 c45">CRIPTOSTORE</span></h3>
+    <p class="c8 c10"><span class="c5">**Desarrollador Blockchain**</span></p>
+    <p class="c8 c12"><span class="c3">Desarrollo de contratos inteligentes. Desarrollo de sistemas basados en blockchain encapsulando y respaldo de informaci&oacute;n de forma inmutable. Desarrollo de sistemas de intercambio de criptomonedas a d&oacute;lar, euros y otros.</span>
+    </p>
+    <hr>
+    <p class="c16 c22"><span class="c3"></span></p>
+    <hr style="page-break-before:always;display:none;">
+    <p class="c8 c10 c28"><span class="c3"></span></p>
+    <p class="c8 c10"><span class="c3">2016-2018</span></p>
+    <h3 class="c7" id="h.2tqtrditjf53"><span class="c6">## </span><span class="c29 c43">Square</span><span
+            class="c21 c6">Web Solution</span></h3>
+    <p class="c8 c10"><span class="c5">**Co-fundador, Ing. Software**</span></p>
+    <p class="c8 c12"><span class="c3">Desarrollo y despliegue de platafromas de ecommerce basadas en woocommerce/shopyfy/opencart. Migraci&oacute;n, estandarizaci&oacute;n y normalizaci&oacute;n de datos. Mobile Native Development en Android, Unity. Dise&ntilde;o y desarrollo de juegos para Android.</span>
+    </p>
+    <hr>
+    <p class="c16 c22"><span class="c3"></span></p>
+    <p class="c8 c10"><span class="c3">2011-2015</span></p>
+    <h3 class="c7" id="h.drd1bqndgsj2"><span class="c6">## M</span><span class="c27">enos</span><span
+            class="c6">M</span><span class="c27">all</span><span class="c21 c6">.com</span></h3>
+    <p class="c8 c10"><span class="c5">**Co-Fundador, Desarrollador FullStack**</span></p>
+    <p class="c8 c12 c30"><span class="c3">Dise&ntilde;o de plataforma escalable de comercio electr&oacute;nico en PHP. Dise&ntilde;o y modelado de base de datos relacional en MySQL para plataforma de comercio electr&oacute;nico. Dise&ntilde;o e integraci&oacute;n frontend bajo responsive design. Integraci&oacute;n de m&oacute;dulos para mejorar la experiencia de usuario en ventas por internet.</span>
+    </p>
+    <hr>
+    <p class="c16 c22"><span class="c3"></span></p>
+    <p class="c8 c10"><span class="c3">2013-2014</span></p>
+    <h3 class="c7" id="h.z689bkmrjzkl"><span class="c6">## &nbsp;Sa</span><span class="c29 c48">fenix</span><span
+            class="c21 c6">&nbsp;S.A.S</span></h3>
+    <p class="c8 c10"><span class="c5">**Project Manager, Arquitecto de software**</span></p>
+    <p class="c8 c12"><span class="c3">Dise&ntilde;o de plataforma escalable para proveer una herramienta mercantil de contacto con la clase media. Arquitectura de software, desarrollo de proyecto. Manejo de equipo de desarrollo multidisciplinario. </span>
+    </p>
+    <hr>
+    <p class="c16 c22"><span class="c3"></span></p>
+    <hr style="page-break-before:always;display:none;">
+    <p class="c8 c10 c28"><span class="c3"></span></p>
+    <p class="c8 c10"><span class="c3">2010-2013</span></p>
+    <h3 class="c7" id="h.bkgoi442ovkf"><span class="c6">## </span><span class="c23">Aquimismito</span><span
+            class="c21 c6">.com</span></h3>
+    <p class="c8 c10"><span class="c5">**CTO/Desarrollador FullStack**</span></p>
+    <p class="c8 c41"><span class="c3">Dise&ntilde;o de plataforma escalable de comercio electr&oacute;nico en PHP basado en Opencart. Dise&ntilde;o y modelado de base de datos relacional en MySQL para plataforma de comercio electr&oacute;nico. Dise&ntilde;o e integraci&oacute;n de m&oacute;dulos para mejorar la UX, dise&ntilde;o y mantenimiento de la infraestructura la cual fu&eacute; escalada a un servicio EC2 en AWS</span>
+    </p>
+    <hr>
+    <p class="c16 c22"><span class="c3"></span></p>
+    <h2 class="c7" id="h.zetg3bhrevyc"><span class="c4">## Educaci&oacute;n Formal</span></h2><h4 class="c20 c10"
+                                                                                                  id="h.wmus2ai8p0i6">
+    <span class="c19">### </span><span class="c21 c19">2005-2009: UNEXPO</span></h4>
+    <p class="c8 c10"><span class="c15">Ingenier&iacute;a Electr&oacute;nica menci&oacute;n Telecomunicaciones (no concluida formalmente)</span>
+    </p>
+    <p class="c8 c10"><span class="c19 c21">### 2000-2005: Escuela T&eacute;cnica Industrial Lara</span></p>
+    <p class="c8 c10"><span class="c15">T&eacute;cnico Medio Industrial en Electr&oacute;nica con menci&oacute;n en Telecomunicaciones.</span>
+    </p>
+    <hr>
+    <p class="c16 c22"><span class="c3"></span></p>
+    <h2 class="c36" id="h.vb0pn4j2hhuz"><span class="c4">## Perfil</span></h2>
+    <p class="c8 c12"><span class="c3">Programador con 15+ a&ntilde;os de experiencia en tecnolog&iacute;a, especializado en tecnolog&iacute;a Web con fuertes bases en Electr&oacute;nica, Marketing Digital, L&oacute;gica de Negocios e IoT.</span>
+    </p>
+    <hr>
+    <p class="c16 c22"><span class="c3"></span></p>
+    <h2 class="c20" id="h.xo595ojd5tjs"><span class="c4">## &Aacute;reas de experiencia</span></h2>
+    <ul class="c1 lst-kix_xn1zzhrtseem-0 start">
+      <li class="c0"><span class="c3">Programador Full-Stack</span></li>
+      <li class="c0"><span class="c3">UX Desing</span></li>
+      <li class="c0"><span class="c3">Mecatr&oacute;nica</span></li>
+      <li class="c0"><span class="c3">Arquitectura de Software</span></li>
+      <li class="c0"><span class="c3">DevOps Jr</span></li>
+      <li class="c0"><span class="c3">Tecnolog&iacute;a Blockchain</span></li>
+      <li class="c0"><span class="c3">bases de datos relacionales y no relacionales</span></li>
+      <li class="c0"><span class="c3">seguridad inform&aacute;tica</span></li>
+    </ul>
+    <hr>
+    <p class="c16 c22"><span class="c3"></span></p>
+    <h2 class="c7" id="h.ppv9k49n5v2h"><span class="c4">## Lenguajes y FrameWorks</span></h2>
+    <ul class="c1 lst-kix_kcl0a6f8ew32-0 start">
+      <li class="c0"><span class="c5">Sr: PHP - HTML - Javascript / NodeJs - CSS - MYSQL - Linux Shell - C - Arduino - Java</span>
+      </li>
+      <li class="c0"><span class="c5">Semi-Sr C++, Java, Less, Pic, Angularjs (1.5.8), NWJS</span></li>
+      <li class="c0"><span class="c5">Jr: Vue JS, AWS-EKS, React, Firebase</span></li>
+    </ul>
+    <hr>
+    <p class="c16 c22"><span class="c21 c6"></span></p>
+    <h2 class="c36" id="h.d5jr04e44wm4"><span class="c4">## Conocimientos extra:</span></h2>
+    <ul class="c1 lst-kix_qbi9twg3822h-0 start">
+      <li class="c0"><span class="c3">Dise&ntilde;o/Animaci&oacute;n 3D (Blender)</span></li>
+      <li class="c0"><span class="c3">Videojuegos</span></li>
+      <li class="c0"><span class="c3">Arduino</span></li>
+      <li class="c0"><span class="c3">Sistemas predictivos / IA (TensorFlow)</span></li>
+      <li class="c0"><span class="c3">Impresi&oacute;n 3D</span></li>
+      <li class="c0"><span class="c3">Seguridad Inform&aacute;tica </span></li>
+      <li class="c0"><span class="c3">Tecnolog&iacute;a Blockchain</span></li>
+      <li class="c0"><span class="c3">Automatizaci&oacute;n de Procesos </span></li>
+      <li class="c0"><span class="c3">Ingl&eacute;s</span></li>
+      <li class="c0"><span class="c3">Portugu&eacute;s</span></li>
+    </ul>
+    <hr>
+    <p class="c16 c22"><span class="c3"></span></p>
+    <p class="c16 c22"><span class="c3"></span></p>
+    <p class="c16 c22"><span class="c3"></span></p>
+    <h2 class="c46" id="h.t5a7oxj922cx"><span class="c4">## Contacto:</span></h2>
+    <p class="c28 c32"><span class="c14"></span></p>
+    <p class="c8"><span class="c18 c29">Correo: </span><span class="c25 c29"><a class="c17"
+                                                                                href="mailto:heilnergarcia@gmail.com">heilnergarcia@gmail.com</a></span><span
+            class="c5"><br>Tlf: +57 314 203 1936</span></p>
+    <p class="c8"><span class="c5">Instagram: </span></p>
+    <ul class="c1 lst-kix_h7uc0zo1ppf1-0 start">
+      <li class="c0"><span class="c25 c29"><a class="c17"
+                                              href="https://www.google.com/url?q=https://www.instagram.com/3dspartan/&amp;sa=D&amp;ust=1565823053168000">https://www.instagram.com/3dspartan/</a></span>
+      </li>
+      <li class="c0"><span class="c25 c29"><a class="c17"
+                                              href="https://www.google.com/url?q=https://www.instagram.com/neverhags/&amp;sa=D&amp;ust=1565823053168000">https://www.instagram.com/neverhags/</a></span>
+      </li>
+    </ul>
+    <h2 class="c46" id="h.phypr9ga8xzr"><span class="c4">## Referencias Personales</span></h2><h4 class="c20"
+                                                                                                  id="h.8q05x09y6nzr">
+    <span class="c21 c19">Lic Wilson Martinez</span></h4>
+    <ul class="c1 lst-kix_xfuo8pz9n06e-0 start">
+      <li class="c39"><span
+              class="c21 c29 c26 c35">Consulting MarAma Business Intelligence | International Speaker</span></li>
+      <li class="c0"><span class="c3">E-mail: wilsonemartinez@gmail.com</span></li>
+      <li class="c0"><span class="c3">Celular: +57 320 2112525</span></li>
+      <li class="c0"><span class="c18">linkedin: </span><span class="c44"><a class="c17"
+                                                                             href="https://www.google.com/url?q=https://www.linkedin.com/in/wilson-e-martinez-a-559775b/&amp;sa=D&amp;ust=1565823053169000">https://www.linkedin.com/in/wilson-e-martinez-a-559775b/</a></span>
+      </li>
+    </ul>
+    <p class="c8 c24"><span class="c14"></span></p><h4 class="c20" id="h.k7zjapb6u6ry"><span class="c21 c19">Ing Juan David Franco</span>
+  </h4>
+    <ul class="c1 lst-kix_xfuo8pz9n06e-0">
+      <li class="c0"><span class="c5">Q-enta Ing DataBase Department Manager</span></li>
+      <li class="c0"><span class="c18">E-mail: </span><span class="c25"><a class="c17"
+                                                                           href="mailto:juan.franco@qenta.co">juan.franco@qenta.co</a></span>
+      </li>
+      <li class="c0"><span class="c18">Celular: +57 300 211 2578<br></span></li>
+    </ul>
+    <h4 class="c20" id="h.si89yx3dwv7x"><span class="c21 c19">Ing Tom&aacute;s Nu&ntilde;ez</span></h4>
+    <ul class="c1 lst-kix_mzwgj06xw71v-0 start">
+      <li class="c0"><span class="c5">Inversionista Independiente</span></li>
+      <li class="c0"><span class="c18">E-mail: </span><span class="c25"><a class="c17"
+                                                                           href="mailto:ixidorcs@gmail.com">ixidorcs@gmail.com</a></span>
+      </li>
+      <li class="c0"><span class="c18">Celular</span><span class="c26">: +57 350 234 3959</span><span class="c26 c31"><br></span>
+      </li>
+    </ul>
+    <h4 class="c20" id="h.xan9s1qwp0ue"><span class="c21 c19">Cristian Ni&ntilde;o</span></h4>
+    <ul class="c1 lst-kix_iknqj81grrs6-0 start">
+      <li class="c9"><span class="c5">CriptoStore</span></li>
+      <li class="c9"><span class="c3">Celular: +57 312 307 8320.</span></li>
+    </ul>
+    <p class="c16"><span class="c3"></span></p>
+    <p class="c32 c28"><span class="c14"></span></p>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
